577e78e80f93d2106e2acb95649175d84c757488
Mariia Borshcheva <mariiaborshcheva@yandex.ru>
2022-01-26 18:38:55 +0300

    tests_2

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -13,12 +13,12 @@ app_name = 'users'
 urlpatterns = [
     path(
         'signup/',
-         views.SignUp.as_view(),
-         name='signup'),
+        views.SignUp.as_view(),
+        name='signup'),
     path(
         'login/',
-         LoginView.as_view(template_name='users/login.html'),
-         name='login'),
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'),
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
