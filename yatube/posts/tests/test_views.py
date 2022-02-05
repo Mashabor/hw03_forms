35714d69319c9ddb0eca3f95b5f48552832050ef
Mariia Borshcheva <mariiaborshcheva@yandex.ru>
2022-02-05 22:31:19 +0300

    test_views

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -20,9 +20,10 @@ class PostViewsTests(TestCase):
         cls.post = Post.objects.create(
             author=cls.user,
             group=cls.group,
-            text='Тестовый заголовок',
+            text='test-text',
             pub_date='04.02.2022'
         )
+
         cls.authorized_client = Client()
         cls.authorized_client.force_login(cls.user)
 
@@ -43,16 +44,76 @@ class PostViewsTests(TestCase):
         
     def test_index_page_show_correct_context(self):
         response = self.authorized_client.get(reverse('posts:index'))
-        first_object = response.context['page_obj'][0]
-        self.assertEqual(first_object, self.post)
+        self.assertEqual(
+            response.context['page_obj'][0], self.post
+        )
 
     def test_group_posts_page_show_correct_context(self):
-        response = self.authorized_client.get(reverse('posts:group_posts', kwargs={'slug': self.group.slug}))
-        first_object = response.context.get('group')[0]
-        post_page_obj = first_object.group
-        self.assertEqual(first_object, self.group)
-        self.assertEqual(response.context['group'], self.post.group)
+        response = self.authorized_client.get(reverse(
+            'posts:group_posts', kwargs={'slug': 'test-slug'}
+        ))
+        self.assertEqual(
+            response.context['page_obj']
+            [0].author.username,
+            'TrueName'
+        )
+        self.assertEqual(
+            response.context['page_obj']
+            [0].text,
+            'test-text'
+        )
+        self.assertEqual(
+            response.context['page_obj']
+            [0].group.title,
+            'test-title'
+        )
+        self.assertEqual(response.context['group'], self.group)
 
+    def test_profile_page_show_correct_context(self):
+        response = self.authorized_client.get(reverse(
+            'posts:profile', kwargs={'username': 'TrueName'}
+        ))
+        self.assertEqual(
+            response.context['page_obj']
+            [0].author.username,
+            'TrueName'
+        )
+        self.assertEqual(
+            response.context['page_obj']
+            [0].text,
+            'test-text'
+        )
+        self.assertEqual(
+            response.context['page_obj']
+            [0].group.title,
+            'test-title'
+        )
+        self.assertEqual(response.context['author'], self.user)
 
-        
-    
+    def test_post_detail_page_show_correct_context(self):
+        response = self.authorized_client.get(reverse(
+            'posts:post_detail', kwargs={'post_id': '1'}
+        ))
+        self.assertEqual(response.context['post'], self.post)
+
+    def test_post_create_show_correct_context(self):
+        response = self.authorized_client.get(reverse('posts:post_create'))
+        form_fields = {
+            'text': forms.fields.CharField
+        }        
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
+
+    def test_post_edit_show_correct_context(self):
+        response = self.authorized_client.get(reverse(
+            'posts:post_edit', kwargs={'post_id': '1'}
+        ))
+        form_fields = {
+            'text': forms.fields.CharField
+        }        
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
